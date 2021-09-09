dc202a386c3e923fe51cf0c0e30dd8bdfc437838
Nicolas Miranda <nicxesh@gmail.com>
2021-09-09 19:10:14 -0300

    Added multiple endspoints and migration/factory for Task controller

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use acidjazz\metapi\MetApi;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+    use MetApi;
+
+    /**
+     * Endpoint getting tasks list
+     */
+    public function index(Request $request)
+    {
+        if ($request->has('completed')) {
+            $tasks = Task::where('completed', true)->get();
+        } else {
+            $tasks = Task::all();
+        }
+
+        return $this->render($tasks);
+    }
+
+    /**
+     * Endpoint to find a specified task from ID
+     */
+    public function find(Request $request, $id)
+    {
+        $task = Task::where('id', $id)->first();
+
+        return $this->render($task);
+    }
+
+    /**
+     * Endpoint to search a specified task from title
+     */
+    public function search(Request $request, $title)
+    {
+        $task = Task::where('title', 'LIKE', '%'.$title.'%')->get();
+
+        return $this->render($task);
+    }
+
+    /**
+     * Endpoint to create a task
+     */
+    public function store(Request $request)
+    {
+        $this
+          ->option('title', 'required|string')
+          ->option('description', 'nullable')
+          ->verify();
+
+        $task = Task::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'completed' => $request->input('completed'),
+        ]);
+
+        return $this->render([
+            'success' => 'true',
+            'message' => 'Task created'
+        ]);
+    }
+
+    /**
+     * Endpoint to update a task
+     */
+    public function update(Request $request, $id)
+    {
+        $this
+          ->option('id', 'required')
+          ->option('title', 'required|string')
+          ->option('description', 'nullable')
+          ->verify();
+
+        $task = Task::where('id', $id)->update([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'completed' => $request->input('completed'),
+        ]);
+
+        return $this->render([
+            'success' => 'true',
+            'message' => 'Task edited'
+        ]);
+    }
+
+    /**
+     * Endpoint to destroy a task
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return $this->render([
+            'success' => 'true',
+            'message' => 'Task deleted'
+        ]);
+    }
+
+    /**
+     * Error response
+     */
+    public function error(): Response|JsonResponse
+    {
+        return $this->render(['forced_error' => $forced_error]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\TasksController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [Controller::class, 'routes'])
+    ->name('route information')
+    ->withoutMiddleware('api');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tasks', [TasksController::class, 'index'])->name('Endpoint getting tasks list');
+Route::get('/tasks/{id}', [TasksController::class, 'find'])->name('Endpoint to find a specified task from ID');
+//Route::get('/tasks/{title}', [TasksController::class, 'search'])->name('Endpoint to search a specified task from title');
+Route::post('/tasks', [TasksController::class, 'store'])->name('Endpoint to create a task');
+Route::put('/tasks/{id}', [TasksController::class, 'update'])->name('Endpoint to destroy a task');
+Route::delete('/tasks/{id}', [TasksController::class, 'destroy'])->name('Endpoint to destroy a task');
